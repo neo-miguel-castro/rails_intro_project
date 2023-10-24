7c83cdf9b6dd74b53be5478174b04d3b5a688afa
Miguel Castro <miguel.castro@neofinancial.com>
2023-10-23 22:13:22 -0500

    Added home and menu layouts and styles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+  def home
+    @articles = []
+    if params[:search].present?
+      @articles = Article.where("title ILIKE ?", "%#{params[:search]}%").includes(:author, :publisher)
+    end
+  end
+
   def about
   end
 end
