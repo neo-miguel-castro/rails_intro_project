9f0003ae225f670af13bd9560c886ffd92c46473
Miguel Castro <miguel.castro@neofinancial.com>
2023-10-23 23:21:19 -0500

    Added categories joint filter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
+
   def home
-    @articles = []
-    if params[:search].present?
-      @articles = Article.where("title ILIKE ?", "%#{params[:search]}%").includes(:author, :publisher)
-      @countries = Country.joins(:article_countries).distinct
+    @categories = Category.all
+    @articles = Article.all.includes(:author, :publisher, :countries)
+
+    if params[:category_id].present?
+      @articles = @articles.joins(:article_categories).where(article_categories: { category_id: params[:category_id] })
     end
+
+    if params[:search].present?
+      @articles = @articles.where("title ILIKE ?", "%#{params[:search]}%")
+    end
+    @countries = Country.joins(:article_countries).distinct
   end
 
   def about
